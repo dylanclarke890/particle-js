8d1febe980b3be52d3c5e75ec4938a8b5c37e6f7
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-27 05:15:24 +0100

    creating canvas

---
+++ script.js
@@ -0,0 +1,18 @@
+function new2dCanvas(id, width, height) {
+  const canvas = document.getElementById(id);
+  const ctx = canvas.getContext("2d");
+  canvas.width = width;
+  canvas.height = height;
+  return [canvas, ctx];
+}
+
+const [canvas, ctx] = new2dCanvas(
+  "canvas",
+  window.innerWidth,
+  window.innerHeight
+);
+
+window.addEventListener("resize", () => {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+});
